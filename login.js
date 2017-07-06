8cdccab970dc67603d74ccfbb911e33f456d61f9
Andy Skrenes <askrenes@users.noreply.github.com>
2017-07-06 14:47:45 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,3 +1,32 @@
+$(document).ready(function() {
+  $("#login_box form input").attr("onfocus", "activate_login();")
+});
+function activate_login() {
+  var login = $("#login_box");  
+  if (login.find('button[type="submit"]').length) {
+    //login is available
+    if ($("#login_box.login_active").length) {
+      //login is already active; do nothing
+    } else {
+      //configure the login animation
+      //dim background
+      $("body").prepend("<div id='login_active_bg' class='login_active' onclick='deactivate_login();'></div>")
+      //move login
+      $("#login_box").addClass("login_active");  
+      $("#login_box form").prepend("<div id='login_heading' class='login-heading'><h4>Partner Log In</h4><span class='login-close' onclick='deactivate_login();'>&times;</span></div>");
+      $("#login_user_name").focus();      
+    }    
+  } else {
+    //user is logged in; forward them to partner home
+    window.location = "home";
+  }
+}
+function deactivate_login() {
+  //remove animated login elements
+  $("#login_active_bg").remove();
+  $("#login_box").removeClass("login_active"); 
+  $("#login_heading").remove();
+}
 function activate_login() {
   var login = $("#login_box");  
   if (login.find('button[type="submit"]').length) {
