9e4bb2b91f66d5a26f315eed118d2230c4fd82ce
Andy Skrenes <askrenes@users.noreply.github.com>
2017-07-10 10:54:43 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#login_box form input").attr("onfocus", "activate_login();")
+  $("#login_box form input").attr("onfocus", "activate_login();$('#login_user_name').focus();")
   $("#login_box button[type='submit']").after("<a href='passwordhelp' class='pw_help glyphicon glyphicon-question-sign'></a>");
   //$("#login_box form").append("<a href='passwordhelp' class='pw_help'>Forgot password?</a>")
 });
@@ -15,8 +15,7 @@ function activate_login() {
       $("body").prepend("<div id='login_active_bg' class='login_active' onclick='deactivate_login();'></div>")
       //move login
       $("#login_box").addClass("login_active");  
-      //$("#login_box form").prepend("<div id='login_heading' class='login-heading'><h4>Partner Log In</h4><span class='login-close' onclick='deactivate_login();'>&times;</span></div>");
-      $("#login_user_name").focus();      
+      //$("#login_box form").prepend("<div id='login_heading' class='login-heading'><h4>Partner Log In</h4><span class='login-close' onclick='deactivate_login();'>&times;</span></div>");          
     }    
   } else {
     //user is logged in; forward them to partner home
